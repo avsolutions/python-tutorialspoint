1398d775d499cb5f8ad45cee9a88119ed89f84e5
anbtan <anbtan@gmail.com>
2016-03-15 18:15:57 +0700

    Implement the greeting

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
-print("Hello Python!!!")
+# title             : hello.py
+# description       : The first Python application
+# author            : An Bui
+# date              : 03/15/2016
+# version           : 1.0
+
+# The first application just to print the text to output
+your_name = input("Please enter your name: ")
+greeting = "Hello %s!\nNice to meet you!!!" % your_name
+print(greeting)
